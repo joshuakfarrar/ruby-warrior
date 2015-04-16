754c60fd17a31ac1347bedf4ec5e022605cd6ca5
Joshua K. Farrar <joshua@sent1nel.me>
2015-04-16 15:37:49 -0400

    level 4 complete

--- zipp-beginner/player.rb
+++ zipp-beginner/player.rb
@@ -1,13 +1,19 @@
 class Player
   def play_turn(warrior)
+    @health ||= warrior.health
+
     if warrior.feel.empty?
-      if warrior.health < 7
-        warrior.rest!
-      else
+      if warrior.health == 20
         warrior.walk!
+      elsif warrior.health < @health
+        warrior.walk!
+      else
+        warrior.rest!
       end
     else
       warrior.attack!
     end
+
+    @health = warrior.health
   end
 end
