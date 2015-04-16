ccf3e298363ed6a7c5ffc20e50c0d4c0d3600d45
Joshua K. Farrar <joshua@sent1nel.me>
2015-04-16 16:10:31 -0400

    level 6 complete

--- zipp-beginner/player.rb
+++ zipp-beginner/player.rb
@@ -1,23 +1,54 @@
 class Player
-  def play_turn(warrior)
-    @health ||= warrior.health
 
-    if warrior.feel.empty?
-      if warrior.health == 20
-        warrior.walk!
-      elsif warrior.health < @health
-        warrior.walk!
+  def rescue_captive
+    if @warrior.feel(:backward).captive?
+      @warrior.rescue!(:backward)
+      @state = :kill_them_all
+    else
+      @warrior.walk!(:backward)
+    end
+  end
+
+  def clear_room
+    if @warrior.feel.empty?
+      if @warrior.health < 10
+        @state = :flee_and_heal
       else
-        warrior.rest!
+        @warrior.walk!
       end
     else
-      if warrior.feel.captive?
-        warrior.rescue!
-      else
-        warrior.attack!
-      end
+      @warrior.attack!
     end
 
-    @health = warrior.health
+    @health = @warrior.health
+  end
+
+  def flee_and_heal
+    if @warrior.health == 20
+      @state = :kill_them_all
+    end
+
+    if @warrior.health < @health
+      @warrior.walk!(:backward)
+    else
+      @warrior.rest!
+    end
+  end
+
+  def play_turn(warrior)
+    @warrior = warrior
+    @health ||= @warrior.health
+    @state ||= :rescue_captive
+
+    case @state
+    when :rescue_captive
+      rescue_captive
+    when :kill_them_all
+      clear_room
+    when :flee_and_heal
+      flee_and_heal
+    end
+
+    @health = @warrior.health
   end
 end
