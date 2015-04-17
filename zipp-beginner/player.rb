5ab4bfe2b8bd1d39029262f23e56941a064e064d
Joshua K. Farrar <joshua@sent1nel.me>
2015-04-17 13:21:01 -0400

    level 7 complete

--- zipp-beginner/player.rb
+++ zipp-beginner/player.rb
@@ -1,48 +1,10 @@
 class Player
-
-  def rescue_captive
-    if @warrior.feel(:backward).captive?
-      @warrior.rescue!(:backward)
-      @state = :kill_them_all
-    else
-      @warrior.walk!(:backward)
-    end
-  end
-
-  def clear_room
-    if @warrior.feel.empty?
-      if @warrior.health < 10
-        @state = :flee_and_heal
-      else
-        @warrior.walk!
-      end
-    else
-      @warrior.attack!
-    end
-
-    @health = @warrior.health
-  end
-
-  def flee_and_heal
-    if @warrior.health == 20
-      @state = :kill_them_all
-    end
-
-    if @warrior.health < @health
-      @warrior.walk!(:backward)
-    else
-      @warrior.rest!
-    end
-  end
-
   def play_turn(warrior)
     @warrior = warrior
-    @health ||= @warrior.health
-    @state ||= :rescue_captive
+
+    @state ||= :kill_them_all
 
     case @state
-    when :rescue_captive
-      rescue_captive
     when :kill_them_all
       clear_room
     when :flee_and_heal
@@ -50,5 +12,45 @@ class Player
     end
 
     @health = @warrior.health
+  end
+
+  def clear_room
+    if warrior_is_facing_a_wall
+      turn_around
+    else
+      fight_forward
+    end
+  end
+
+  def warrior_is_facing_a_wall
+    @warrior.feel.wall?
+  end
+
+  def turn_around
+    @warrior.pivot!(:backward)
+  end
+
+  def fight_forward
+    if @warrior.feel.empty?
+      @warrior.walk!
+    else
+      @warrior.attack!
+    end
+
+    if @warrior.health < 10
+      @state = :flee_and_heal
+    end
+  end
+
+  def flee_and_heal
+    if @warrior.health < @health
+      @warrior.walk!(:backward)
+    else
+      @warrior.rest!
+    end
+
+    if @warrior.health >= 20
+      @state = :kill_them_all
+    end
   end
 end
