63091305366a5c1942e2bb378a944b1310b8dfa2
Joshua K. Farrar <joshua@sent1nel.me>
2015-04-20 08:23:49 -0400

    intermediate level 3 complete

--- zipp-intermediate/player.rb
+++ zipp-intermediate/player.rb
@@ -2,25 +2,98 @@ class Player
   def play_turn(warrior)
     @warrior = warrior
 
-    direction_of_stairs = @warrior.direction_of_stairs
+    @action ||= Actions::RescueCaptive.new
 
-    space = warrior.feel(direction_of_stairs)
+    next_action = @action.act(warrior)
 
-    case space.to_s
-    when 'Sludge', 'Thick Sludge'
-      monster = Monster.from_space(space)
-      attack(monster, direction_of_stairs)
-    else
-      walk(direction_of_stairs)
-    end
-  end
-
-  def attack(monster, direction)
-    @warrior.attack!(direction)
+    @action = next_action
   end
 
   def walk(direction)
     @warrior.walk!(direction)
+  end
+end
+
+module Actions
+  class Base
+    def act(warrior)
+      raise Actions::ActNotImplementedError.new("Actions must have an act method!")
+    end
+  end
+
+  class RescueCaptive < Actions::Base
+    def act(warrior)
+      warrior.rescue!(:right)
+      Actions::FightToStairs.new
+    end
+  end
+
+  class FightToStairs < Actions::Base
+    def act(warrior)
+      @warrior = warrior
+
+      if @warrior.health <= 9
+        action = FleeAndHeal.new
+        action.act(@warrior)
+        return action
+      end
+
+      direction_of_stairs = @warrior.direction_of_stairs
+      space = @warrior.feel(direction_of_stairs)
+
+      case space.to_s
+      when 'Sludge', 'Thick Sludge'
+        monster = Monster.from_space(space)
+        attack(monster, direction_of_stairs)
+      else
+        @warrior.walk!(direction_of_stairs)
+      end
+
+      self
+    end
+
+    def attack(monster, direction)
+      @warrior.attack!(direction)
+    end
+  end
+
+  class FleeAndHeal < Actions::Base
+    def act(warrior)
+      @warrior = warrior
+      @health ||= @warrior.health
+      @fled ||= false
+
+      if !@fled
+        flee
+      else
+        if fully_healed?
+          action = FightToStairs.new
+          return action.act(@warrior)
+        else
+          heal
+        end
+      end
+
+      @health = @warrior.health
+      self
+    end
+
+    def in_danger?
+      @warrior.health > @health
+    end
+
+    def flee
+      @warrior.walk!(:right)
+      @fled = true
+    end
+
+    def fully_healed?
+      @warrior.health == 20
+    end
+
+    def heal
+      @warrior.rest!
+    end
   end
 end
 
